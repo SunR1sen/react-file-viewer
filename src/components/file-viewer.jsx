c78c755cdeb4665d01ff705186f4d3ddfe74d89f
Dmitry Boldyr <d.boldyr@onlystudio.org>
2021-02-09 17:12:06 +0300

    run prettier

--- src/components/file-viewer.jsx
+++ src/components/file-viewer.jsx
@@ -1,12 +1,11 @@
 // Copyright (c) 2017 PlanGrid, Inc.
 
-import React, { Component } from 'react';
-import PropTypes from 'prop-types';
-import 'styles/main.scss';
-import withFetching from './fetch-wrapper';
+import React, { Component } from "react";
+import PropTypes from "prop-types";
+import "styles/main.scss";
+import withFetching from "./fetch-wrapper";
 
 import {
-  CsvViewer,
   DocxViewer,
   VideoViewer,
   XlsxViewer,
@@ -15,7 +14,7 @@ import {
   UnsupportedViewer,
   PhotoViewerWrapper,
   AudioViewer,
-} from './drivers';
+} from "./drivers";
 
 class FileViewer extends Component {
   constructor(props) {
@@ -26,7 +25,7 @@ class FileViewer extends Component {
   }
 
   componentDidMount() {
-    const container = document.getElementById('pg-viewer');
+    const container = document.getElementById("pg-viewer");
     const height = container ? container.clientHeight : 0;
     const width = container ? container.clientWidth : 0;
     this.setState({ height, width });
@@ -40,27 +39,27 @@ class FileViewer extends Component {
         });
         return withFetching(XlsxViewer, newProps);
       }
-      case 'jpg':
-      case 'jpeg':
-      case 'gif':
-      case 'bmp':
-      case 'png': {
+      case "jpg":
+      case "jpeg":
+      case "gif":
+      case "bmp":
+      case "png": {
         return PhotoViewerWrapper;
       }
-      case 'pdf': {
+      case "pdf": {
         return PDFViewer;
       }
-      case 'docx': {
+      case "docx": {
         return DocxViewer;
       }
-      case 'mp3': {
+      case "mp3": {
         return AudioViewer;
       }
-      case 'webm':
-      case 'mp4': {
+      case "webm":
+      case "mp4": {
         return VideoViewer;
       }
-      case 'wexbim': {
+      case "wexbim": {
         return XBimViewer;
       }
       default: {
@@ -74,7 +73,11 @@ class FileViewer extends Component {
     return (
       <div className="pg-viewer-wrapper">
         <div className="pg-viewer" id="pg-viewer">
-          <Driver {...this.props} width={this.state.width} height={this.state.height} />
+          <Driver
+            {...this.props}
+            width={this.state.width}
+            height={this.state.height}
+          />
         </div>
       </div>
     );
